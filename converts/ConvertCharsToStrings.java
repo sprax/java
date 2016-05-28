b7194e5e09dbc808f1141cdf91b0767a3c7dafd3
Sprax Lines <sprax@comcast.net>
2016-05-28 01:57:02 -0400

    conversions

---
+++ converts/ConvertCharsToStrings.java
@@ -0,0 +1,105 @@
+package sprax.converts;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import sprax.Sx;
+import sprax.Sz;
+
+public class ConvertCharsToStrings
+{
+    
+    static int sNumIters = 100000;
+    
+    // Times:
+    // 1. Initialization of "s" outside the loop
+    // 2. Init of "s" inside the loop
+    // 3. newFunction() actually checks the string length,
+    // so the function will not be optimized away by the hotstop compiler
+    
+    @Test
+    // Fastest: 237ms / 562ms / 2434ms
+    public static void testCacheStrings()
+    {
+        // Cache all possible Char strings
+        String[] char2string = new String[Character.MAX_VALUE];
+        for (char i = Character.MIN_VALUE; i < Character.MAX_VALUE; i++) {
+            char2string[i] = Character.toString(i);
+        }
+        
+        for (int x = 0; x < sNumIters; x++) {
+            char[] s = "abcdefg".toCharArray();
+            for (int i = 0; i < s.length; i++) {
+                doSomething(char2string[s[i]]);
+            }
+        }
+    }
+    
+    @Test
+    // Fast: 1687ms / 1725ms / 3382ms
+    public static void testCharToString() throws Exception
+    {
+        for (int x = 0; x < sNumIters; x++) {
+            String s = "abcdefg";
+            for (int i = 0; i < s.length(); i++) {
+                // Fast: Creates new String objects, but does not copy an array
+                doSomething(Character.toString(s.charAt(i)));
+            }
+        }
+    }
+    
+    @Test
+    // Very fast: 1331 ms/ 1414ms / 3190ms
+    public static void testSubstring() throws Exception
+    {
+        for (int x = 0; x < sNumIters; x++) {
+            String s = "abcdefg";
+            for (int i = 0; i < s.length(); i++) {
+                // The fastest! Reuses the internal char array
+                doSomething(s.substring(i, i + 1));
+            }
+        }
+    }
+    
+    @Test
+    // Slowest: 2525ms / 2961ms / 4703ms
+    public static void testNewString() throws Exception
+    {
+        char[] value = new char[1];
+        for (int x = 0; x < sNumIters; x++) {
+            char[] s = "abcdefg".toCharArray();
+            for (int i = 0; i < s.length; i++) {
+                value[0] = s[i];
+                // Slow! Copies the array
+                doSomething(new String(value));
+            }
+        }
+    }
+    
+    private static void doSomething(String string)
+    {
+        // Do something with the one-character string
+        Assert.assertEquals(1, string.length());
+    }
+    
+    public static int unit_test()
+    {
+        String testName = ConvertCharsToStrings.class.getName() + ".unit_test";
+        Sz.begin(testName);
+        
+        long begTime, endTime, runTime;
+        
+        begTime = System.currentTimeMillis();
+        testCacheStrings();
+        endTime = System.currentTimeMillis();
+        runTime = endTime - begTime;
+        Sx.puts("done: " + runTime);
+        Sz.end(testName, 0);
+        return 0;
+    }
+    
+    public static void main(String[] args)
+    {
+        unit_test();
+    }
+}
---
+++ converts/HexStringToNum.java
@@ -0,0 +1,105 @@
+package sprax.converts;
+
+import sprax.Sx;
+import sprax.Sz;
+import sprax.maths.DoubleToFraction;
+import sprax.maths.NumberAlgo;
+
+public class HexStringToNum 
+{
+	/**
+	 * Java defines its char set with 7 chars between '9' and 'A',
+	 * so just map those to zero, and we can use array look-up, no hashing.
+	 * The size of this table is 23 ints.
+	 */
+	static final int[] sHexCharToDecVal = { 
+		 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+		    -1, -2, -3, -4, -5, -6, -7,
+		10, 11, 12, 13, 14, 15,
+	};
+
+	/**
+	 * Somewhat forgiving implementation of htoi: return 0 as result from null string, 
+	 * non-hex chars and any other garbage in. 
+	 * @param hexStr
+	 * @return
+	 */
+	public static int htoi(final String hexStr)
+	{
+		if (hexStr == null || hexStr.length() == 0)
+			return 0;
+
+		int retVal = 0;		// return value
+		int decVal = 0;
+		int strPos = 0;
+		boolean isNegative = false;
+		if (hexStr.charAt(0) == '-')
+		{
+			isNegative = true;
+			strPos = 1;
+		}
+		for ( ; strPos < hexStr.length(); strPos++)
+		{
+			int idx = hexStr.charAt(strPos) - '0';
+			try {
+				decVal = sHexCharToDecVal[idx];
+			} 
+			catch (ArrayIndexOutOfBoundsException ex) {
+				Sx.format("Error: bad char(%c) at position %d of string: %s\n", hexStr.charAt(strPos), strPos, hexStr);
+				return 0;
+			}
+			if (decVal < 0)
+			{
+				Sx.format("Error: bad char(%c) at position %d of string: %s\n", hexStr.charAt(strPos), strPos, hexStr);
+				return 0;
+			}
+			retVal = (retVal << 4) + decVal;
+		}
+		if (isNegative)
+			return -retVal;
+		else
+			return  retVal;
+	}
+	
+	private static void test_htoi(String hexStr)
+	{
+        Sx.format("htoi(%s) \t%d\n", hexStr, htoi(hexStr));		
+	}
+	
+    public static int unit_test()
+    {
+        String testName = HexStringToNum.class.getName() + ".unit_test";
+        Sz.begin(testName);
+
+        Sx.puts("1 << 4 is " + (1 << 4));
+        
+        Sx.format("A - 0 is: %d\n",   (int)('A' - '0'));
+        for (int j = '0'; j <= 'F'; j++)
+        	Sx.format("  %c", (char)(j));
+        Sx.puts();	
+
+        test_htoi("0");
+        test_htoi("-0");
+        test_htoi("0-");
+        test_htoi("1");
+        test_htoi("-1");
+        test_htoi("A");
+        test_htoi("11");
+        test_htoi("1B");
+        test_htoi("B1");
+        test_htoi("FF");
+        test_htoi("-");
+        test_htoi("-FF");
+        test_htoi("FG");
+        test_htoi("F@F");
+        
+        test_htoi("FEDCBA");
+        test_htoi("FEDCBA9876543210");
+        test_htoi("FEDCBA9876543210123456789ABCDEF");
+         
+        Sz.end(testName, 0);
+        return 0;
+    }
+    
+    public static void main(String[] args)  { unit_test(); }
+}
