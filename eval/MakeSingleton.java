e5a35224c91179794e5c0f4fbf6424c6b1a31626
Sprax Lines <sprax@comcast.net>
2016-05-31 10:03:26 -0400

    eval

---
+++ eval/MakeSingleton.java
@@ -0,0 +1,15 @@
+package sprax.eval;
+
+public class MakeSingleton 
+{
+    private String message;
+    
+    public MakeSingleton() {
+        this.message = "Hello world!";
+    }
+    
+    public void saySomething() {
+        System.out.println(message);
+    }
+    
+}
