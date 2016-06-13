7b22d4e5135af9910d9ad3b746a52983f2fbf247
Sprax Lines <sprax@comcast.net>
2016-06-12 22:23:23 -0400

    rename

--- questions/LongestGreaterSubStr.java
+++ questions/LongestGreaterSubStr.java
@@ -4,7 +4,7 @@ import sprax.Sz;
 import sprax.sprout.Sx;
 
 /** function(s) to return longest substring lexicographically greater than the whole string. */
-public class LongestGreaterSubstring
+public class LongestGreaterSubStr
 {
 
     /**
@@ -13,7 +13,7 @@ public class LongestGreaterSubstring
      * First calculate it. Then iterate the array and each time get Failure[i] 
      * which is the end of the longest prefix that is suffix to the substring[0...i] 
      * We check if next character a[i +1] is greater than arr[Failure[i]+1].
-     * That's is the place where we have first mismatch.
+     * That's is the place where we have first mismatch.j
      * In case it is greater, we have found the longest substring that is 
      * lexicographic greater than our string S.
      * Otherwise we continues to iterate the array with incremented i. 
@@ -63,12 +63,12 @@ public class LongestGreaterSubstring
     
     public static int testOne(String str, String expected) {
         String result = longestGreaterSubstring(str);
-        Sx.format("%s ==> %s\n", str, result);
+        Sx.format("'%s' ==> '%s'\n", str, result);
         return Sz.oneWrong(result, expected);
     }
     
     public static int unit_test() {
-        String testName = LongestGreaterSubstring.class.getName() + ".unit_test";
+        String testName = LongestGreaterSubStr.class.getName() + ".unit_test";
         Sz.begin(testName);
         int numWrong = 0;
         
---
+++ test/Sv.java
@@ -0,0 +1,23 @@
+package sprax.test;
+
+import sprax.Sz;
+
+/**
+ * static utility methods for testing, debugging, etc.
+ *
+ */
+public class Sv 
+{
+
+    public static int unit_test()
+    {
+        String testName = Sv.class.getName() + ".unit_test";
+        Sz.begin(testName);
+        int numWrong = 0;
+ 
+        Sz.end(testName, numWrong);
+        return numWrong;
+    }
+
+    public static void main(String args[]) { unit_test(); }
+}
