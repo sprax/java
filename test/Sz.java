a4cfc4374763f4f8634e6bbd0f725c0e6b1544ba
Sprax Lines <sprax@comcast.net>
2016-06-15 10:15:01 -0400

    dogfood

--- test/Sz.java
+++ test/Sz.java
@@ -8,7 +8,7 @@ import sprax.sprout.Sx;
  */
 public class Sz 
 {
-    public static int wrong(boolean result) { return result ? 0 : 1; }
+    public static int oneIfFalse(boolean result) { return result ? 0 : 1; }
     public static int oneWrong(boolean result, boolean expected) { return result == expected ? 0 : 1; }
     public static int oneWrong(int result, int expected) { return result == expected ? 0 : 1; }
     public static int oneWrong(String result, String expected) { return result.equals(expected) ? 0 : 1; }
@@ -25,32 +25,34 @@ public class Sz
         return numWrong == 0 ? "PASS" : "FAIL"; 
     }
 
-    public static int unit_test()
-    {
-        String testName = Sz.class.getName() + ".unit_test";
-        Sx.format("BEGIN %s\n", testName);
-
-        int numWrong = 0;
-        numWrong += oneWrong(true, true);
-        numWrong += oneWrong(false, false);
-        numWrong += oneWrong(0, 0);
-        numWrong += oneWrong(-1, 1 - 2);
-
-        Sx.format("END   %s,  wrong: %d, %s\n", testName, numWrong, passFail(numWrong));
-        return 0;
-    }
-
     public static void begin(String testName) {
         Sx.format("BEGIN %s\n", testName);  
     }
 
     public static void end(String testName, int numWrong) {
-        Sx.format("END   %s,  wrong %d, %s\n", testName, numWrong, Sz.passFail(numWrong));  
+        Sx.format("END   %s,  wrong %d,  %s\n", testName, numWrong, Sz.passFail(numWrong));  
     }
 
     public static void ender(String testName, int numCases, int numWrong) {
-        Sx.format("END   %s,  right %d, wrong %d, %s\n"
+        Sx.format("END   %s,  right %d,  wrong %d, %s\n"
                 , testName, numCases - numWrong, numWrong, Sz.passFail(numWrong));  
+    }
+
+
+    public static int unit_test()
+    {
+        String testName = Sz.class.getName() + ".unit_test";
+        begin(testName);
+
+        int numWrong = 0;
+        numWrong += oneIfFalse(true);
+        numWrong += oneWrong(true, true);
+        numWrong += oneWrong(false, false);
+        numWrong += oneWrong(0, 0);
+        numWrong += oneWrong(-1, 1 - 2);
+
+        end(testName, numWrong);
+        return numWrong;
     }
 
     public static void main(String args[]) { unit_test(); }
