ae1fd4156d9f63076198fb038241b9878bdfbed3
Sprax Lines <sprax@comcast.net>
2016-07-08 22:57:10 -0400

    ratios

--- random/Amoebas.java
+++ random/Amoebas.java
@@ -6,30 +6,49 @@ import sprax.test.Sz;
 import sprax.arrays.ArrayAlgo;
 import sprax.sprout.Sx;
 
+/**
+ * Example from Wikipedia article on Martingales:
+ * https://en.wikipedia.org/wiki/Martingale_(probability_theory)
+ * 
+ * Suppose each amoeba either splits into two amoebas, with probability p, 
+ * or eventually dies, with probability 1 − p. 
+ * Let Xn be the number of amoebas surviving in the nth generation 
+ * (in particular Xn = 0 if the population has become extinct by that time). 
+ * Let r be the probability of eventual extinction. (Finding r as a function 
+ * of p is an instructive exercise. Hint: The probability that the descendants
+ * of an amoeba eventually die out is equal to the probability that either of 
+ * its immediate offspring dies out, given that the original amoeba has split.) 
+ * Then {\displaystyle \{\,r^{X_{n}}:n=1,2,3,\dots \,\}} \{\,r^{X_{n}}:n=1,2,3,\dots \,\}
+ * is a martingale with respect to { Xn: n = 1, 2, 3, ... }.
+ * 
+ * One can also ask, What's the probability of the population lasting N generations?
+ * 
+ * @author sprax    2016.07.08
+ */
 public class Amoebas 
 {
     long population;
     long generation;
     double splitProb;
-    Random rng;
+    Random random;
 
-    Amoebas() 
+    public Amoebas() 
     { this(1); }
     
-    Amoebas(long startNum) 
+    public Amoebas(long startNum) 
     { this(startNum, 0.5); }
 
-    Amoebas(long startNum, double splitProb) 
+    public Amoebas(long startNum, double splitProb) 
     { this(startNum, splitProb, System.currentTimeMillis()); }
     
-    Amoebas(long startNum, double splitProb, long seed) 
+    public Amoebas(long startNum, double splitProb, long seed) 
     { this(startNum, splitProb, new Random(seed)); }
     
-    Amoebas(long startNum, double splitProb, Random rng) 
+    public Amoebas(long startNum, double splitProb, Random rng) 
     { 
         this.population = startNum;
         this.splitProb = splitProb;
-        this.rng = rng;
+        this.random = rng;
     }
      
     public boolean generate()
@@ -37,7 +56,7 @@ public class Amoebas
         generation++;
         for (long j = 0, oldPop = population; j < oldPop; j++)
         {
-            double next = rng.nextDouble();
+            double next = random.nextDouble();
             if (next <= splitProb)
                 population += 2;
             else 
@@ -52,10 +71,20 @@ public class Amoebas
         Sz.begin(testName);
         int numWrong = 0;
         
-        Amoebas amoebas = new Amoebas(1, 0.615);
-        while (amoebas.population > 0) {
-            amoebas.generate();
-            Sx.format("%d :  %d\n",  amoebas.generation, amoebas.population);
+        double goldenRatio = 1.618033988749894848204586834365638117720;
+
+        Amoebas am = new Amoebas(1, 1.0/goldenRatio);
+        
+        long oldPop = am.population;
+        long targetGeneration = 32;
+        long begTime = System.currentTimeMillis();
+        for (int j = 0; j < targetGeneration && oldPop > 0; j++) {
+            am.generate();
+            long newPop = am.population;
+            double ratio = (double) newPop / oldPop;
+            long millis = System.currentTimeMillis() - begTime;
+            Sx.format("%3d :  %12d     %f   %8d\n",  am.generation, am.population, ratio, millis);
+            oldPop = newPop;
         }
         
         Sz.end(testName, numWrong);
