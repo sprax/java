1a57c3f8c8b44a79fb8e8cbcd4f39489ade4176c
Sprax Lines <sprax@comcast.net>
2016-07-15 18:07:44 -0400

    hacker insert

---
+++ sorts/InsertSortStdin.java
@@ -0,0 +1,71 @@
+/*************************************************************************
+ * https://www.hackerrank.com/challenges/insertionsort2
+ *************************************************************************/
+package sprax.sorts;
+
+import java.io.*;
+import java.util.*;
+import sprax.test.Sz;
+
+public class InsertSortStdin<T extends Comparable<T>> implements SortInt
+{
+    @Override
+    public void sort(int[] array) {
+        insertionSortArray(array);
+    }
+    
+    public static void insertionSortArray(int[] ar)
+    {       
+        for (int j = 1; j < ar.length; j++)
+            insertSortHead(ar, j);
+    }  
+    
+    public static void insertSortHead(int[] ar, int idx) {
+        int val = ar[idx];
+        while (--idx >= 0 && val < ar[idx]) {
+            ar[idx+1] = ar[idx];
+        }
+        ar[idx+1] = val;
+        printArray(ar);
+    }
+    
+    public static void sortStdin() 
+    {
+        try (Scanner in = new Scanner(System.in)) {
+            int s = in.nextInt();
+            int[] ar = new int[s];
+            for(int i=0;i<s;i++){
+                ar[i]=in.nextInt(); 
+            }
+            insertionSortArray(ar);    
+        }    
+    }
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+    
+    public static int unit_test(int level) 
+    {
+        String testName = InsertSortStdin.class.getName() + ".unit_test";
+        Sz.begin(testName);
+        int numWrong = 0;
+        int verbose = 1;
+        
+        InsertSortStdin<Integer> intSorter = new InsertSortStdin<>();
+        numWrong += SortUtil.test_sort_random_int_array(intSorter, 32, 100, 0L, verbose);
+        
+        Sz.end(testName, numWrong);
+        return numWrong;
+    }
+    
+    public static void main(String[] args) 
+    {
+        unit_test(1);
+    }
+}
+
+
