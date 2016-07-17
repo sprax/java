edbeaaae5f43ae7ef256372d1f960a85eb855671
Sprax Lines <sprax@comcast.net>
2016-07-17 09:25:02 -0400

    todo: compare

--- questions/MinDiffPairs.java
+++ questions/MinDiffPairs.java
@@ -7,10 +7,12 @@
 
 package sprax.questions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableMap;
+import java.util.Scanner;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -43,12 +45,80 @@ public class MinDiffPairs
 		}
     }
     
- 
+    static class CompDiffPair implements Comparator<DiffPair>
+    {
+		@Override
+		public int compare(DiffPair arg0, DiffPair arg1) {
+			return arg0.compareTo(arg1);
+		}	
+    }
+    
+    public static int[] scanIntArray() 
+    {    
+        try (Scanner in = new Scanner(System.in)) {
+            
+            int numEntries = in.nextInt();
+            int arr[] = new int[numEntries];
+            for (int j = 0; j < numEntries; j++) {
+            	arr[j] = in.nextInt();
+            }
+            return arr;
+        } catch(Throwable ex)
+        {
+        	System.out.println("scanIntArray returning empty array; caught:\n" + ex);
+        	return new int[0];
+        }
+    }
+
+    public static DiffPair[] scanDiffPairArray() 
+    {    
+        try (Scanner in = new Scanner(System.in)) {
+            
+            int numEntries = in.nextInt();
+            DiffPair dp[] = new DiffPair[numEntries];
+            for (int j = 0; j < numEntries; j++) {
+            	int xx = in.nextInt();
+            	int yy = in.nextInt();
+            	dp[j] = new DiffPair(xx, yy);
+            }
+            return dp;
+        } catch(Throwable ex)
+        {
+        	System.out.println("scanIntArray returning empty array; caught:\n" + ex);
+        	return new DiffPair[0];
+        }
+    }
+
+    public static int showMinDiffPairs(DiffPair diffPairs[])
+    {
+    	ArrayList<DiffPair> dpList = new ArrayList<>();
+    	int minDiff = Integer.MAX_VALUE;
+    	for (DiffPair dp : diffPairs) {
+    		int diff = dp.absDiff;
+    		if (minDiff < diff)
+    			continue;
+    		if (minDiff > diff) {
+    			minDiff = diff;
+    			dpList.clear();
+    		}
+    		dpList.add(dp);
+    	}
+    	dpList.sort(CompDiffPair::compare);
+    	for (DiffPair dp : dpList)
+    		System.out.format("%d %d ", dp.xx, dp.yy);
+    	System.out.println();
+    	return dpList.size();
+    }
+
+    
     public static int unit_test(int level)
     {
         String testName = MinDiffPairs.class.getName() + ".unit_test";
         Sz.begin(testName);
         int numWrong = 0;
+        
+        DiffPair[] dpArray = scanDiffPairArray();
+        showMinDiffPairs(dpArray);
         
         Sz.end(testName, numWrong);
         return numWrong;
