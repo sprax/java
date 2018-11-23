13fc52181c7ffae40454f6b6f4404231c6074adc
Sprax Lines <sprax@comcast.net>
2018-11-22 21:52:26 -0500

    optimize _opt

--- models/Histarea.java
+++ models/Histarea.java
@@ -6,29 +6,32 @@ public class Histarea
 {
     static int histarea_opt(int histogram[], int length)
     {
+        if (length < 3)
+            return 0;
         int area = 0;
-        if (length > 2) {
-            int maxFromLeft[] = new int[length];
-            int height, maxHeight = Integer.MIN_VALUE;
-            for (int j = 0; j < length; j++) {  // Don't skip the last entry.
-                height = histogram[j];
-                if (maxHeight < height)
-                    maxHeight = height;
-                maxFromLeft[j] = maxHeight;
-            }
-            maxHeight = Integer.MIN_VALUE;
-            for (int j = length; --j > 0;) {   // Do skip the first entry.
-                height = histogram[j];
-                if (maxHeight < height)
-                    maxHeight = height;
-                if (maxHeight > maxFromLeft[j])
-                    area += maxFromLeft[j] - height;
-                else
-                    area += maxHeight - height;
-            }
+        int lenMinus1 = length - 1;
+        int maxFromLeft[] = new int[lenMinus1];
+        int leftMax = maxFromLeft[0] = histogram[0];
+        for (int j = 1; j < lenMinus1; j++) {  // Skip the first and last entries.
+            int height = histogram[j];
+            if (leftMax < height)
+                leftMax = height;
+            maxFromLeft[j] = leftMax;
+        }
+        int rightMax = histogram[lenMinus1];
+        for (int j = lenMinus1; --j > 0;) {   // Skip the last and first entries.
+            int height = histogram[j];
+            if (rightMax < height)
+                rightMax = height;
+            leftMax = maxFromLeft[j];
+            if (rightMax > leftMax)
+                area += leftMax - height;
+            else
+                area += rightMax - height;
         }
         return area;
     }
+
     static int histarea(int histogram[], int length)
     {
         int area = 0;
@@ -62,8 +65,6 @@ public class Histarea
     {
         Sx.puts(Histarea.class.getName() + ".main");
         final String programName = args.length > 0 ? args[0] : defProgramName;
-        
-        
         int histograms[][] = {
                 {    0,    -1,      2,     3,     -4,     4,     4,     2,     7,     0 },
                 {   -1,     2,     32,    32,     -4,     4,    44,     2,    38,     0 },
