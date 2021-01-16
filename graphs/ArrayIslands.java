34b58a359f1675712169b2c0dc612b9a931523ea
sprax <sprax@comcast.net>
2021-01-16 00:30:21 -0500

    ArrayIslands.java/format

--- graphs/ArrayIslands.java
+++ graphs/ArrayIslands.java
@@ -1,3 +1,7 @@
+// @file: ArrayIslands.java
+// @auth: Sprax Lines
+// @date: 2016-05-28 09:38:51 Sat 28 May
+
 package sprax.graphs;
 
 import java.awt.Point;
@@ -10,73 +14,77 @@ import sprax.arrays.Arrays1d;
 import sprax.sprout.Sx;
 
 /**
- * Given a matrix containing 0 and 1. Consider 1 as 'Land' and 0 as 'Water'. Find out the number of
- * 'Islands' in the matrix. That is, set of all adjacent 1 will make up for an island.
- * 
+ * Given a matrix containing 0 and 1.  Consider 1 as 'Land' and 0 as 'Water'.
+ Find out the number of
+ * 'Islands' in the matrix.  That is, set of all adjacent 1 will make up for an
+ island.
+ *
  * For example:
- * 
+ *
  * [ 0 1 1 0 1 ] [ 1 1 1 0 0 ] [ 0 0 0 1 1 ] [ 1 0 0 1 0 ]
- * 
- * This problem has 4 islands. ( consider set of 1s, vertically, horizontally and diagonally ).
- * 
- * Note: Classify this as a graph problem, not an array problem, because the initial array is used
- * only to specify a graph; the graph does not cover the whole array. In other words, the problem
+ *
+ * This problem has 4 islands. ( consider set of 1s, vertically,
+ horizontally and diagonally ).
+ *
+ * Note: Classify this as a graph problem, not an array problem,
+ because the initial array is used
+ * only to specify a graph; the graph does not cover the whole array.
+ In other words, the problem
  * domain is a graph constructed from only part the array.
- */
+*/
 
 class Anode
 {
-    int         mValue;    // original array value
-    Point       mPoint;    // node location (x = row, y = col)
+    int mValue;            // original array value
+    Point mPoint;          // node location (x = row, y = col)
     List<Point> mAdj;      // nearest neighbors
-    int         mComponent;
-    
+    int mComponent;
+
     Anode(int value, Point point, List<Point> adjList, int component)
     {
-        mValue = value;
-        mPoint = point;
-        mAdj = adjList;
+        mValue     = value;
+        mPoint     = point;
+        mAdj       = adjList;
         mComponent = component;
     }
 }
 
 class Agraph
 {
-    int         mComponentCount;
+    int mComponentCount;
     List<Anode> mNodes;
-    
+
     Agraph(int compCount, List<Anode> nodes)
     {
         mComponentCount = compCount;
-        mNodes = nodes;
+        mNodes          = nodes;
     }
 }
 
 public class ArrayIslands
 {
-    int         count;          // initially zero
-    boolean     mMarked[][];    // initially all false
-    final int   mArray[][];
-    final int   mRows;
-    final int   mCols;
-    int         mComponentCount;
+    int count;                  // initially zero
+    boolean mMarked[][];        // initially all false
+    final int mArray[][];
+    final int mRows;
+    final int mCols;
+    int mComponentCount;
     List<Anode> mNodes;
-    Agraph      mGraph;
-    
+    Agraph mGraph;
+
     /**
-     * Constructor uses BFS to build a minimal graph, counting the connected components along the
-     * way
-     */
+     * Constructor uses BFS to build a minimal graph, counting the connected
+     * components along the way
+    */
     public ArrayIslands(final int array[][])
     {
         // FIXME: handle case of 1x1 array?
         assert (array != null && array.length > 1);
-        mRows = array.length;
-        mCols = array[0].length;
-        mArray = array;
+        mRows   = array.length;
+        mCols   = array[0].length;
+        mArray  = array;
         mMarked = new boolean[mRows][mCols];
-        mNodes = new ArrayList<Anode>();
-        
+        mNodes  = new ArrayList<Anode>();
         for (int j = 0; j < mRows; j++)
         {
             int rowJ[] = mArray[j];
@@ -93,16 +101,17 @@ public class ArrayIslands
         }
         mGraph = new Agraph(mComponentCount, mNodes);
     }
-    
+
     protected void exploreNewIsland(int row, int col)
     {
-        Point newPoint = new Point(row, col);
-        Queue<Point> queue = new LinkedList<Point>();
+        Point        newPoint = new Point(row, col);
+        Queue<Point> queue    = new LinkedList<Point>();
         queue.add(newPoint);
-        do {
-            Point point = queue.remove();
+        do
+        {
+            Point       point   = queue.remove();
             List<Point> adjList = getAdjListRightDownLeftUp(point);
-            Anode node = new Anode(mArray[row][col], point, adjList, mComponentCount);
+            Anode       node    = new Anode(mArray[row][col], point, adjList, mComponentCount);
             mNodes.add(node);
             for (Point adj : adjList)
             {
@@ -110,30 +119,35 @@ public class ArrayIslands
             }
         } while (!queue.isEmpty());
     }
-    
+
     private List<Point> getAdjListRightDownLeftUp(Point point)
     {
         ArrayList<Point> adjList = new ArrayList<Point>();
         int row = point.x;
         int col = point.y + 1; // right
-        if (col < mCols) // in bounds right?
+        if (col < mCols) { // in bounds right?
             addNewPointIfPosVal(adjList, row, col);
+        }
         ++row;
         --col; // down
-        if (row < mRows) // in bounds bottom?
+        if (row < mRows) { // in bounds bottom?
             addNewPointIfPosVal(adjList, row, col);
+        }
         --row;
         --col; // left
-        if (col >= 0) // in bounds left?
+        if (col >= 0) { // in bounds left?
             addNewPointIfPosVal(adjList, row, col);
+        }
         --row;
         ++col; // up
-        if (row >= 0) // in bounds top?
+        if (row >= 0) { // in bounds top?
             addNewPointIfPosVal(adjList, row, col);
+        }
         return adjList;
     }
-    
-    /** If this point position is new (unmarked) and positively values, add it. */
+
+    /** If this point position is new (unmarked) and positively values, add it.
+    */
     private void addNewPointIfPosVal(List<Point> adjList, int x, int y)
     {
         if (mMarked[x][y] == false && mArray[x][y] > 0)
@@ -142,58 +156,60 @@ public class ArrayIslands
             adjList.add(new Point(x, y));
         }
     }
-    
+
     public static int test_ArrayIslands()
     {
-        int errors = 0;
+        int       errors    = 0;
         final int array[][] = new int[][] {
-                { 0, 1, 1, 0, 2, 0, 0, 0, 0, 0 },
-                { 1, 1, 1, 0, 0, 0, 3, 3, 0, 3 },
-                { 0, 0, 1, 0, 0, 0, 3, 0, 3, 3 },
-                { 4, 0, 0, 5, 5, 0, 3, 3, 3, 0 },
-                { 0, 6, 0, 5, 0, 0, 3, 0, 3, 0 }
+            { 0, 1, 1, 0, 2, 0, 0, 0, 0, 0 },
+            { 1, 1, 1, 0, 0, 0, 3, 3, 0, 3 },
+            { 0, 0, 1, 0, 0, 0, 3, 0, 3, 3 },
+            { 4, 0, 0, 5, 5, 0, 3, 3, 3, 0 },
+            { 0, 6, 0, 5, 0, 0, 3, 0, 3, 0 }
         };
-        ArrayIslands ai = new ArrayIslands(array);
-        int nComp = ai.mComponentCount;
-        if (nComp != 6)
+        ArrayIslands ai    = new ArrayIslands(array);
+        int          nComp = ai.mComponentCount;
+        if (nComp != 6) {
             errors++;
+        }
         Sx.puts("Found " + nComp + " islands (connected components in:");
         Sx.putsArray(array);
         Sx.puts();
-        
+
         final int matrix[][] = new int[][] {
-                { 0, 1, 0, 2, 2, 0, 0, 0 },
-                { 3, 0, 4, 0, 2, 0, 5, 5 },
-                { 0, 0, 4, 0, 0, 0, 5, 0 },
-                { 6, 6, 0, 0, 7, 0, 0, 0 },
-                { 6, 0, 0, 0, 0, 8, 8, 8 },
-                { 6, 6, 6, 0, 0, 0, 0, 8 },
-                { 6, 0, 6, 0, 0, 8, 8, 8 },
-                { 6, 0, 6, 0, 0, 8, 0, 8 },
-                { 6, 6, 0, 0, 0, 8, 8, 8 }
+            { 0, 1, 0, 2, 2, 0, 0, 0 },
+            { 3, 0, 4, 0, 2, 0, 5, 5 },
+            { 0, 0, 4, 0, 0, 0, 5, 0 },
+            { 6, 6, 0, 0, 7, 0, 0, 0 },
+            { 6, 0, 0, 0, 0, 8, 8, 8 },
+            { 6, 6, 6, 0, 0, 0, 0, 8 },
+            { 6, 0, 6, 0, 0, 8, 8, 8 },
+            { 6, 0, 6, 0, 0, 8, 0, 8 },
+            { 6, 6, 0, 0, 0, 8, 8, 8 }
         };
-        ai = new ArrayIslands(matrix);
+        ai    = new ArrayIslands(matrix);
         nComp = ai.mComponentCount;
-        if (nComp != 8)
+        if (nComp != 8) {
             errors++;
+        }
         Sx.puts("Found " + nComp + " islands (connected components in:");
         Sx.putsArray(matrix);
         Sx.puts();
-        
+
         return errors;
     }
-    
+
     public static int unit_test(int level)
     {
         String testName = Arrays1d.class.getName() + ".unit_test";
         Sx.puts(testName + " BEGIN\n");
-        
+
         int errors = test_ArrayIslands();
-        
+
         Sx.puts(testName + " ENDED with " + errors + " errors.");
         return errors;
     }
-    
+
     public static void main(String[] args)
     {
         unit_test(0);
